8ab095d5ca5dec9929ab289df6fec89ce2831109
bryan heinz <git@bryanheinz.com>
2025-11-19 11:17:10 -0600

    added script

---
+++ _install_configs.zsh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+ln -s ~/.files/_configs/editorconfig ~/.editorconfig
+ln -s ~/.files/_configs/gitconfig ~/.gitconfig
+ln -s ~/.files/_configs/logrc ~/.logrc
+ln -s ~/.files/_configs/pylintrc ~/.pylintrc
+ln -s ~/.files/_configs/tmux.conf ~/.tmux.conf
