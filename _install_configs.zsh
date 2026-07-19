ce31c4a594d62c7960e59fa99607e8a581876315
bryan heinz <git@bryanheinz.com>
2026-07-18 22:00:58 -0500

    updated to check for pre-existing files

--- _install_configs.zsh
+++ _install_configs.zsh
@@ -1,7 +1,28 @@
 #!/bin/zsh
 
-ln -s ~/.files/_configs/editorconfig ~/.editorconfig
-ln -s ~/.files/_configs/gitconfig ~/.gitconfig
-ln -s ~/.files/_configs/logrc ~/.logrc
-ln -s ~/.files/_configs/pylintrc ~/.pylintrc
-ln -s ~/.files/_configs/tmux.conf ~/.tmux.conf
+create_link () {
+    # $1==src, $2==dst
+    if [[ -L "$2" ]]; then
+        # a link already exists, re-link
+        echo "re-linking ${2:t}"
+        rm "$2"
+        ln -s "$1" "$2"
+    elif [[ -e "$2" ]]; then
+        # a file exists, but isn't a link
+        # backup existing file and link
+        backupFN="${2}-$(date +%s)"
+        echo "found existing config, backing up ${backupFN} and creating link: ${2:t}"
+        mv "$2" "${backupFN}"
+        ln -s "$1" "$2"
+    else
+        # no file or link exists
+        echo "linking ${2:t}"
+        ln -s "$1" "$2"
+    fi
+}
+
+create_link ~/.files/_configs/editorconfig ~/.editorconfig
+create_link ~/.files/_configs/gitconfig ~/.gitconfig
+create_link ~/.files/_configs/logrc ~/.logrc
+create_link ~/.files/_configs/pylintrc ~/.pylintrc
+create_link ~/.files/_configs/tmux.conf ~/.tmux.conf
